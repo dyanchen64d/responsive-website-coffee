906b46af7a3edf30bbae58fa07c64b07eba2943c
chen.zhang <zhangchen64d@gmail.com>
2023-03-02 23:52:18 +0900

    update

--- main.js
+++ main.js
@@ -10,7 +10,37 @@ const scrollHeader = () => {
 window.addEventListener('scroll', scrollHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollup = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  // when the scroll is higher than 350
+  window.scrollY >= 350
+    ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollup);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+  const scrollY = window.scrollY;
+
+  sections.forEach((curr) => {
+    const sectionHeight = curr.offsetHeight,
+      sectionTop = curr.offsetTop - 58,
+      sectionId = curr.getAttribute('id'),
+      sectionsClass = document.querySelector(
+        '.nav__menu a[href*=' + sectionId + ']'
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link');
+    } else {
+      sectionsClass.classList.remove('active-link');
+    }
+  });
+};
+
+window.addEventListener('scroll', scrollActive);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
